aa608642e48b7f59ca8cd59c3a255e3973a8a425
MrTj458 <mrtj458@gmail.com>
2018-09-19 17:06:19 -0600

    Added PostView.js

--- client/src/components/PostView.js
+++ client/src/components/PostView.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+import {Divider, Header, Image, Container, Table} from 'semantic-ui-react'
 
-const PostView = () => (
-	<div>Post View</div>
+const PostView = ({post = {}}) => (
+	<Container>
+		<Link to="/posts">View All Posts</Link>
+		<Header as="h3" textAlign="center">{post.title}</Header>
+		<Header as="h6" textAlign="center">{post.author}</Header>
+		<p>{post.body}</p>
+	</Container>
 )
+
+const mapStateToProps = (state, props) => {
+	return {post: state.posts.find(p => p.id === parseInt(props.match.params.id))}
+}
 
 export default PostView
