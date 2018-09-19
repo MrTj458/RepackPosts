39d6223ed5c60bcc73f183f9dffb231bc55f29ad
MrTj458 <mrtj458@gmail.com>
2018-09-19 16:35:05 -0600

    Fixed typo

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import NoMatch from './components/NoMatch'
 
 const App = () => (
   <Switch>
-    <Route exact path="/" component={home} />
+    <Route exact path="/" component={Home} />
     <Route exact path="/posts" component={Posts} />
     <Route exact path="posts/:id" component={PostView} />
     <Route component={NoMatch} />
