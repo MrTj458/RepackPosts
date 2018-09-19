44bd718c928bcdcb068ed5c9712bd5a19ec3b894
MrTj458 <mrtj458@gmail.com>
2018-09-19 17:04:55 -0600

    Added store to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Switch, Route} from 'react-router-dom'
+import store from './store'
 import Home from './components/Home'
 import Posts from './components/Posts'
 import PostView from './components/PostView'
 import NoMatch from './components/NoMatch'
 
 const App = () => (
-  <Switch>
+  <Switch store={store}>
     <Route exact path="/" component={Home} />
     <Route exact path="/posts" component={Posts} />
     <Route exact path="posts/:id" component={PostView} />
