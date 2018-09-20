a1806c1e261ec2c6b573b7a4aae3f3e7047e91f0
MrTj458 <mrtj458@gmail.com>
2018-09-20 10:54:54 -0600

    Fixed another warning

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react'
+import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {Container, Header, Card} from 'semantic-ui-react'
@@ -6,30 +6,28 @@ import {Container, Header, Card} from 'semantic-ui-react'
 class Posts extends React.Component {
 	posts = () => {
 		return (
-			this.props.posts.map(post => {
-				<Fragment>
-					<Card key={post.id}>
-						<Card.Content>
-							<Card.Header>
-								{post.title}
-							</Card.Header>
-							<Card.Meta>
-								<span>
-									{post.author}
-								</span>
-							</Card.Meta>
-							<Card.Description>
-								{post.body}
-							</Card.Description>
-						</Card.Content>
-						<Card.Content extra>
-							<Link to={`/posts/${post.id}`}>
-								View Post
-							</Link>
-						</Card.Content>
-					</Card>
-				</Fragment>
-			})
+			this.props.posts.map(post => 
+				<Card key={post.id}>
+					<Card.Content>
+						<Card.Header>
+							{post.title}
+						</Card.Header>
+						<Card.Meta>
+							<span>
+								{post.author}
+							</span>
+						</Card.Meta>
+						<Card.Description>
+							{post.body}
+						</Card.Description>
+					</Card.Content>
+					<Card.Content extra>
+						<Link to={`/posts/${post.id}`}>
+							View Post
+						</Link>
+					</Card.Content>
+				</Card>
+			)
 		)
 	}
 
